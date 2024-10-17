3998ad9ab19551b8f10edb0cdbbf625fb2bb4c68
JustinLucero07 <justinluceroxd04@gmail.com>
2024-10-17 16:35:10 -0500

    Primero archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CursoComponent } from './components/curso/curso.component';
+import { FormularioCursoComponent } from './components/formulario-curso/formulario-curso.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CursoComponent,FormularioCursoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Practica-AplicacionWebInteractiva';
+
+  ngOnInit(): void {
+    
+  }
 }
