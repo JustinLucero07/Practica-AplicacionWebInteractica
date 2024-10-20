f314f82215c7b32d1d5b5de1a56be935e5670f23
JustinLucero07 <justinluceroxd04@gmail.com>
2024-10-20 12:00:57 -0500

    Se realiza la imagen de fondo pero sin exito ya que no carga de manera correcta en el css

--- src/app/services/servicio-curso.service.ts
+++ src/app/services/servicio-curso.service.ts
@@ -9,11 +9,10 @@ export class ServicioCursoService {
 
   constructor(private http: HttpClient) {}
 
-  // Función para obtener una imagen aleatoria de perro
   async getFotoPerro2(): Promise<any> {
     try {
       const data = await firstValueFrom(this.http.get<any>('https://dog.ceo/api/breeds/image/random'));
-      return data.message; // Devuelve la URL de la imagen del perro
+      return data.message;
     } catch (error) {
       console.error('Error al obtener la imagen del perro:', error);
       return '';
